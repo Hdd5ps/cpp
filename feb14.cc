653573b088d143e8dc750d3ec08a7a246c93e202
Hdd5ps <hdd5ps@virginia.edu>
2025-02-14 17:49:39 +0000

    feb14

---
+++ feb14.cc
@@ -0,0 +1,27 @@
+/*
+Enumerated types:
+    int values - sort of
+
+    enum gradeType{LABS, ASSIGNMENTS, QUIZZES, EXAMS, FINAL_EXAM};
+                    0       1           2         3     4
+
+    for (int i = 0; i <(int)FINAL_EXAM; i++){ 
+        (gradeType) i => if i -> 3 // actually looking at exams
+
+    }
+    
+    started gathering all lab grades (i = 0)
+    i = 1, prompt for assignments
+    i = 2, prompt for quizzes
+    i = 3, prompt for exams
+
+    Programming Assignments #4
+
+    - make sure you're doing your own work
+    - if sharing code (at least 1 line), it is an Honor code violation
+
+
+    e
+
+    
+*/
