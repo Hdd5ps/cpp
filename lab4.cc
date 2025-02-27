95f586abffcaac4977c004cf8a65a2f9f6af6c8a
Hdd5ps <hdd5ps@virginia.edu>
2025-02-27 18:52:08 +0000

    lab4

---
+++ lab4.cc
@@ -0,0 +1,15 @@
+#include "lab4Completed.h"
+
+
+int main(){
+   double average = 0;
+   calculateCurrentGrade(average);
+   //average represents total points earned across all grade components,
+   //except for the final exam
+   //printAverage prints out the total number of points divided by the
+   //percentage of the grade already known (everything but the final)
+   printAverage(average, FINAL_EXAM);
+   //prints what is necessary to make an A, B, C, or D.
+   printNeededScores(average);
+   return 0;
+}
