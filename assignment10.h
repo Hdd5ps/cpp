9fd79ebe8cfe0ef662e68fed09adaa9cb28da760
Hdd5ps <hdd5ps@virginia.edu>
2025-04-18 01:08:05 +0000

    clasS

--- assignment10.h
+++ assignment10.h
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<cstdlib>
 #include<ctime>
+#include<iomanip>
 #include<vector>
 #include "assignment6.h"
 #include "assignment7.h" //reusing files again?  Hmmm...
@@ -17,13 +18,103 @@ public:
    //Constructor with one argument, default argument; name of the player
    //the default argument is the empty string ""
    //Calls the setters
+   Player (string name = ""){
+      setName(name);
+      setAtBats(0);
+      setSingles(0);
+      setDoubles(0);
+      setTriples(0);
+      setHomers(0);
+      setWalks(0);
+      setSacflies(0);
+   }
       
    //define getters and setters
+   //setters
+   void setName(string n){
+      name = n;
+   }
+
+   void setAtBats(int ab){
+      atBats = ab;
+   }
+
+   void setWalks(int w){
+      walks = w;
+   }
+
+   void setSacflies(int sf){
+      sacflies = sf;
+   }
+
+   void setSingles(int s){
+      singles = s;
+   }
+
+   void setDoubles(int d){
+      doubles = d;
+   }
+
+   void setTriples(int t){
+      triples = t;
+   }
+
+   void setHomers(int h){
+      homers = h;
+   }
+
+   //getters
+   string getName(){
+      return name;
+   }
+
+   int getAtBats(){
+      return atBats;
+   }
+
+   int getWalks(){
+      return walks;
+   }
+
+   int getSacflies(){
+      return sacflies;
+   }
+
+   int getSingles(){
+      return singles;
+   }
+
+   int getDoubles(){
+      return doubles;
+   }
+
+   int getTriples(){
+      return triples;
+   }
+
+   int getHomers(){
+      return homers;
+   }
 
    //the ofstream argument passed in refers to the file being printed to.
-   void print(ofstream&);
-
-
+   void print(ofstream& outFile){
+      //print the name of the player>>
+      outFile << setw(20) << left << name;
+      //print the stats
+      outFile << setw(5) << atBats;
+      outFile << setw(5) << singles;
+      outFile << setw(5) << doubles;
+      outFile << setw(5) << triples;
+      outFile << setw(5) << homers;
+      outFile << setw(5) << walks;
+      outFile << setw(5) << sacflies;
+      //print the batting average
+      outFile << setw(8) << fixed << setprecision(3) << battingAverage();
+      //print the on base percentage
+      outFile << setw(8) << fixed << setprecision(3) << onBasePercentage();
+      //print the slugging percentage
+      outFile << setw(8) << fixed << setprecision(3) << sluggingPercentage();
+   }
 
 private:
 
@@ -32,13 +123,31 @@ private:
 
    //calculates batting average the same as before, just the methodology
    //will be different.
-   double battingAverage();
+   double battingAverage(){
+      //if the player has no at bats, return 0.0
+      if (atBats == 0)
+         return 0.0;
+      //otherwise, return the average
+      return (double)(singles + doubles + triples + homers) / atBats;
+   }
    
    //calculates obp, just the methodology is different
-   double onBasePercentage();
+   double onBasePercentage(){
+      //if the player has no at bats, return 0.0
+      if (atBats + walks == 0)
+         return 0.0;
+      //otherwise, return the average
+      return (double)(singles + doubles + triples + homers + walks) / (atBats + walks);
+   }
 
    //same story with slugging percentage 
    double sluggingPercentage(){
+      //if the player has no at bats, return 0.0
+      if (atBats == 0)
+         return 0.0;
+      //otherwise, return the average
+      return (double)(singles + (2 * doubles) + (3 * triples) + (4 * homers)) / atBats;
+   }
       
    string name; //player's name
    int atBats, walks, sacflies; //stats to keep up with
@@ -48,15 +157,65 @@ private:
 
    
 //reads names from roster.txt and stores them to objects in the vector.
-void initialize(vector<Player>&, const int);
+void initialize(vector<Player>& lineup, const int LINEUP_SIZE){
+   srand(time(0)); //seed the random number generator
+   //open the file
+   ifstream inFile("roster.txt");
+   //loop through the vector, getting names from the file
+   for (int i = 0; i < LINEUP_SIZE; i++){
+      string name;
+      getline(inFile, name);
+      //create a new player object with the name
+      Player p(name);
+      //push it into the vector
+      lineup.push_back(p);
+   }
+   inFile.close();
+}
    
 
 //prints out all of the batters' results, formatted like previous assignments,
 //to a file called a10results.txt   
-void printPlayerStats(vector<Player>);
+void printPlayerStats(vector<Player> lineup){
+   //open the file
+   ofstream outFile("a10results.txt");
+   //print the header
+   outFile << setw(20) << left << "Player"
+           << setw(5) << "ab"
+           << setw(5) << "1b"
+           << setw(5) << "2b"
+           << setw(5) << "3b"
+           << setw(5) << "hr"
+           << setw(5) << "bb"
+           << setw(5) << "sf"
+           << setw(8) << "avg"
+           << setw(8) << "obp"
+           << setw(8) << "slug" << endl;
+   //loop through the vector, printing each player's stats
+   for (int i = 0; i < lineup.size(); i++){
+      lineup[i].print(outFile);
+      outFile << endl;
+   }
+   outFile.close();
+}
 
 //similar to previous versions of playBall, the argument this time being
 //a vector of players.  As with previous versions
-void playBall(vector<Player>&);
+void playBall(vector<Player>& lineup){
+   //initialize the play result
+   PlayResult play;
+   GeneratedPlayType playOutcome;
+   //loop through the innings
+   for (int inning = 0; inning < 9; inning++){
+      int outs = 0;
+      //loop through the players
+      for (int i = 0; i < lineup.size(); i++){
+         //generate a random play
+         playOutcome = generatePlay();
+         //update the player's stats based on the play result
+         updateStatLine(playOutcome, lineup, i);
+      }
+   }
+}
 
 #endif
--- assignment9.h
+++ assignment9.h
@@ -6,36 +6,29 @@
 #include <iomanip>
 #include "assignment6.h"
 #include "assignment7.h"
-// hmmm...reusing an old file again here?
 using namespace std;
 
 #ifndef ASSIGNMENT9_H
 #define ASSIGNMENT9_H
 
 struct Player
-{                // each player...
-   string name;  //...has a name (from roster.txt)
-   int atBats;   // at-bats
-   int singles;  // singles
-   int doubles;  // doubles
-   int triples;  // triples
-   int homers;   // home runs
-   int walks;    // walks
-   int sacflies; // sacflies
+{
+   string name;  // Player's name
+   int atBats;   // At-bats
+   int singles;  // Singles
+   int doubles;  // Doubles
+   int triples;  // Triples
+   int homers;   // Home runs
+   int walks;    // Walks
+   int sacflies; // Sacrifice flies
 };
 
-// needed for srand() call and initializing all lineup names and
-// all values set to zero.
-// names of the batters come from roster.txt
+// Initialize the lineup from roster.txt
 void initialize(Player *lineup, const int LINEUP_SIZE)
 {
-   // initialize the random number generator
-   srand(time(0));
-   // open the file roster.txt
+   srand(time(0)); // Seed the random number generator
    ifstream inFile("roster.txt");
 
-   // loop through the array of players, assigning names and
-   // initializing all values to zero
    for (int i = 0; i < LINEUP_SIZE; i++)
    {
       getline(inFile, lineup[i].name);
@@ -50,79 +43,51 @@ void initialize(Player *lineup, const int LINEUP_SIZE)
    inFile.close();
 }
 
-// same calculation as before, just collecting the information in
-// a different way.
+// Calculate batting average
 double battingAverage(Player p)
 {
-   // If the player has no at-bats, return 0.0
    if (p.atBats == 0)
-   {
       return 0.0;
-   }
-   // Correct calculation of batting average
-   double avg = (double)(p.singles + p.doubles + p.triples + p.homers) / (double)p.atBats;
-   return avg;
+   return (double)(p.singles + p.doubles + p.triples + p.homers) / p.atBats;
 }
 
-// same story for on-base as batting average
+// Calculate on-base percentage
 double onBasePercentage(Player p)
 {
-   // If the player has no at-bats or walks, return 0.0
    if (p.atBats + p.walks == 0)
-   {
       return 0.0;
-   }
-   // Correct calculation of on-base percentage
-   double obp = (double)(p.singles + p.doubles + p.triples + p.homers + p.walks) / (double)(p.atBats + p.walks);
-   return obp;
+   return (double)(p.singles + p.doubles + p.triples + p.homers + p.walks) / (p.atBats + p.walks);
 }
 
-// same story for slugging percentage
+// Calculate slugging percentage
 double sluggingPercentage(Player p)
 {
-   // If the player has no at-bats, return 0.0
    if (p.atBats == 0)
-   {
       return 0.0;
-   }
-   // Correct calculation of slugging percentage
-   double slg = (double)(p.singles + (2 * p.doubles) + (3 * p.triples) + (4 * p.homers)) / (double)p.atBats;
-   return slg;
+   return (double)(p.singles + (2 * p.doubles) + (3 * p.triples) + (4 * p.homers)) / p.atBats;
 }
 
-// print out the player's stats to a file specified through the
-// ostream argument.
-// called by printPlayerStats
+// Print player stats to a file
 void printPlayer(Player p, ofstream &outFile)
 {
-   // print the player's name
-   outFile << setw(20) << left << p.name;
-   // print the player's stats
-   outFile << setw(5) << p.atBats
+   outFile << setw(20) << left << p.name
+           << setw(5) << p.atBats
            << setw(5) << p.singles
            << setw(5) << p.doubles
            << setw(5) << p.triples
            << setw(5) << p.homers
            << setw(5) << p.walks
-           << setw(5) << p.sacflies;
-   // print the player's batting average, on-base percentage, and slugging percentage
-   outFile << setw(8) << battingAverage(p)
+           << setw(5) << p.sacflies
+           << setw(8) << battingAverage(p)
            << setw(8) << onBasePercentage(p)
            << setw(8) << sluggingPercentage(p) << endl;
 }
 
-// prints the header of each column, as well as each player's stats
-// relies on printPlayerStats for a good bit of its functionality.
-// writes to the file a9results.txt
+// Print all player stats to a9results.txt
 void printPlayerStats(const Player *lineup, const int LINEUP_SIZE)
 {
-   // open the file a9results.txt
    ofstream outFile("a9results.txt");
-   // print out the header
-   // Set formatting for the output file
    outFile << fixed << setprecision(3);
-
-   // Print the headers
    outFile << setw(20) << left << "Player"
            << setw(5) << "ab"
            << setw(5) << "1b"
@@ -135,131 +100,109 @@ void printPlayerStats(const Player *lineup, const int LINEUP_SIZE)
            << setw(8) << "obp"
            << setw(8) << "slug" << endl;
 
-   // use printPlayer to print out each player's stats
    for (int i = 0; i < LINEUP_SIZE; i++)
    {
       printPlayer(lineup[i], outFile);
    }
-   // close the file
    outFile.close();
 }
 
-// after a batter has their plate appearance, update their stats,
-// based on the PlayResult; the third argument corresponds to
-// a batter in the lineup
+// Update player stats based on the play result
 void updateStatLine(PlayResult p, Player *pl, const int index)
 {
-   // Update the player's stats based on the PlayResult
    switch (p)
    {
    case SINGLE:
       pl[index].singles++;
-      pl[index].atBats++; // Singles count as an at-bat
+      pl[index].atBats++;
       break;
    case DOUBLE:
       pl[index].doubles++;
-      pl[index].atBats++; // Doubles count as an at-bat
+      pl[index].atBats++;
       break;
    case TRIPLE:
       pl[index].triples++;
-      pl[index].atBats++; // Triples count as an at-bat
+      pl[index].atBats++;
       break;
    case HOME_RUN:
       pl[index].homers++;
-      pl[index].atBats++; // Home runs count as an at-bat
+      pl[index].atBats++;
       break;
    case WALK:
-      pl[index].walks++; // Walks do not count as an at-bat
+      pl[index].walks++;
       break;
    case SAC_FLY:
-      pl[index].sacflies++; // Sacrifice flies do not count as an at-bat
+      pl[index].sacflies++;
       break;
    case FLY_OUT:
    case GROUND_OUT:
    case STRIKE_OUT:
    case POP_OUT:
    case DOUBLE_PLAY:
-      pl[index].atBats++; // Outs count as an at-bat
+      pl[index].atBats++;
       break;
    default:
       break;
    }
 }
 
-// playBall is the main function of the program.  It simulates a
-// similar functionality to playBall from the previous assignment's
-// playBall.  The major difference here is the use of the pointer for
-// the array of batters.
+// Simulate a nine-inning game
 void playBall(Player *lineup)
 {
-   // Define the size of the lineup
    const int LINEUP_SIZE = 9;
 
    // Initialize the play result
    PlayResult play;
-   // Initialize the play outcome
    GeneratedPlayType playOutcome;
 
-   // Loop through the nine innings
    for (int inning = 0; inning < 9; inning++)
    {
-      // Initialize the number of outs
       int outs = 0;
+      int inningHits = 0;
+      int inningWalks = 0;
 
       cout << "Inning " << inning + 1 << ": " << endl;
 
-      // Loop through the batters in a circular manner
       int batter = 0;
       while (outs < 3)
       {
-         // Roll the dice to determine the play
-         PlayResult play = roll(playOutcome); // Use roll() from assignment6.h
-
+         PlayResult play = roll(playOutcome); // Simulate the play
          cout << "Up to bat: " << lineup[batter].name << "\n";
          cout << "Play: ";
-         printPlayResult(play); // Use printPlayResult() from assignment6.h
+         printPlayResult(play);
 
          updateStatLine(play, lineup, batter);
-         // Update the batter's stats
 
-         // Check if the play is an out
-         if (play == FLY_OUT || play == SAC_FLY || play == GROUND_OUT || play == STRIKE_OUT || play == POP_OUT || play == DOUBLE_PLAY)
+         if (play == SINGLE || play == DOUBLE || play == TRIPLE || play == HOME_RUN)
+         {
+            inningHits++;
+         }
+         else if (play == WALK)
+         {
+            inningWalks++;
+         }
+         else if (play == FLY_OUT || play == SAC_FLY || play == GROUND_OUT || play == STRIKE_OUT || play == POP_OUT || play == DOUBLE_PLAY)
          {
             outs++;
          }
 
-
-         // Move to the next batter
          batter = (batter + 1) % LINEUP_SIZE;
       }
 
-      // Calculate total hits and walks for the inning
-      int totalHits = 0;
-      int totalWalks = 0;
-      for (int i = 0; i < LINEUP_SIZE; i++)
-      {
-         totalHits += lineup[i].singles + lineup[i].doubles + lineup[i].triples + lineup[i].homers;
-         totalWalks += lineup[i].walks;
-      }
+      cout << "End of inning " << inning + 1 << ".\n";
+      cout << "Walks: " << inningWalks << endl;
+      cout << "Hits: " << inningHits << endl;
 
-      // Display inning summary
-      cout << "End of inning.\n";
-      cout << "Walks: " << totalWalks << endl;
-      cout << "Hits: " << totalHits << endl;
-      cout << endl
-           << endl;
-
-      // Prompt the user to continue or not
       char choice;
       cout << "Continue? (y/n): ";
       cin >> choice;
-      cout << endl;
-
       if (choice == 'n' || choice == 'N')
       {
-         return; // Exit the game
+         break;
       }
    }
+
+   printPlayerStats(lineup, LINEUP_SIZE);
 }
 
 #endif
